2e635919ca6dba4beee65ad9068af271c27f31ab
Terence <limterence12@gmail.com>
2022-02-24 10:14:38 +1100

    script components
    Added filler script

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,21 +2,17 @@ import Timetable_mini from './dashboard/timetable_mini';
 import Calendar_mini from './dashboard/calendar_mini';
 import Motivational_quote from './dashboard/motivational_quote';
 import Focus_mini from './dashboard/focus_mini';
-import { Helmet } from 'react-helmet'; { /*Remember to add a full timetable!*/ }
+import {Helmet} from 'react-helmet';
+{/*Remember to add a full timetable!*/}
 
-{ /*This is the dashboard. Includes a timetable, calendar motivational quote display and a focus display*/ }
+{/*This is the dashboard. Includes a timetable, calendar motivational quote display and a focus display*/}
 export default function Dashboard() {
-    return ( <
-        div className = 'grid_wrapper' >
-        <
-        Timetable_mini / >
-        <
-        Calendar_mini / >
-        <
-        Motivational_quote / >
-        <
-        Focus_mini / >
-        <
-        /div>
+    return (
+        <div className='grid_wrapper'>
+          <Timetable_mini />
+          <Calendar_mini />
+          <Motivational_quote />
+          <Focus_mini />
+        </div>
     );
 }
--- src/components/timetable/timetable_daily.js
+++ src/components/timetable/timetable_daily.js
@@ -2,21 +2,17 @@ import Timetable_mini from './timetable_mini';
 import Calendar_mini from './calendar_mini';
 import Motivational_quote from './motivational_quote';
 import Focus_mini from './focus_mini';
-import { Helmet } from 'react-helmet'; { /*Remember to add a full timetable!*/ }
+import {Helmet} from 'react-helmet';
+{/*Remember to add a full timetable!*/}
 
-{ /*This is the dashboard. Includes a timetable, calendar motivational quote display and a focus display*/ }
+{/*This is the dashboard. Includes a timetable, calendar motivational quote display and a focus display*/}
 export default function Dashboard() {
-    return ( <
-        div className = 'grid_wrapper' >
-        <
-        Timetable_mini / >
-        <
-        Calendar_mini / >
-        <
-        Motivational_quote / >
-        <
-        Focus_mini / >
-        <
-        /div>
+    return (
+        <div className='grid_wrapper'>
+          <Timetable_mini />
+          <Calendar_mini />
+          <Motivational_quote />
+          <Focus_mini />
+        </div>
     );
 }
